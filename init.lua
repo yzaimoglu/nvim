e93b4a61f80db1e6f8b23a8672333cecd5691abe
Yagizalp Zaimoglu <yz@yagi.sh>
2024-12-09 20:46:04 +0100

    keymaps: add find files and find buffers

--- init.lua
+++ init.lua
@@ -412,6 +412,9 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      vim.keymap.set('n', '<Space>f', builtin.find_files, { desc = '[F]iles' })
+      vim.keymap.set('n', '<Space>b', builtin.buffers, { desc = '[B]uffers' })
+
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
